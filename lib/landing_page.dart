eeccb6263e3b736050d1dabeedf7c1b86c1f9ade
Racso1624 <fer10_lb@outlook.com>
2022-06-10 22:17:54 -0600

    Se agrega landing page

---
+++ lib/landing_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget{
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      
+    );
+  }
+}
